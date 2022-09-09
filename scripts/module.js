0bb001adf89e3cb9aa05085b55bfc74e1490b7fa
caelwarner <52902343+CaelWarner@users.noreply.github.com>
2022-09-09 15:26:10 -0700

    Update to v10
    - Module now works in FoundryVTT v10
    - Added toggle to enable darkness activation range so that users can disable it to be able to manually show and hide tiles

--- scripts/module.js
+++ scripts/module.js
@@ -9,13 +9,13 @@ class DarknessActivatedTiles {
     static async onRenderTileConfig(config, html, tile) {
         if (!config.object.getFlag(this.ID, "darkness"))
             // Can't use setFlag because at this point the tile doesn't have an id yet
-            tile.data.flags[this.ID] = { "darkness": { "min": 0, "max": 1 }};
+            tile.data.flags[this.ID] = { "darkness": { "enabled": false, "min": 0, "max": 1 }};
 
         const contents = await renderTemplate(`modules/${this.ID}/templates/tile-darkness-range.hbs`, tile);
         html.find(`div[data-tab="basic"] .form-group`).last().after(contents);
 
         config.activateListeners(html);
-        config.setPosition({ height: "auto", width: "auto" });
+        config.setPosition({ height: "auto", width: "500" });
     }
 
     static onLightingRefresh(lighting) {
@@ -23,10 +23,14 @@ class DarknessActivatedTiles {
             return;
         }
 
-        canvas.background.tiles.forEach(tile => {
-            const darknessRange = tile.document.getFlag(this.ID, "darkness");
+        canvas.scene.tiles.forEach(tile => {
+            const darknessRange = tile.getFlag(this.ID, "darkness");
 
-            tile.document.update({ hidden: lighting.darknessLevel < darknessRange.min || lighting.darknessLevel > darknessRange.max })
+            if (darknessRange.enabled) {
+                console.log(canvas.scene.darkness < darknessRange.min || canvas.scene.darkness > darknessRange.max);
+
+                tile.update({ hidden: canvas.scene.darkness < darknessRange.min || canvas.scene.darkness > darknessRange.max });
+            }
         });
     }
 }
